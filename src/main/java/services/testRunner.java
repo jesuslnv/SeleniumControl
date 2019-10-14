cc916caf3a4c4db1ad483add100aedb953833a28
Jesus Neira <jneiravizcarra@gmail.com>
2019-10-14 14:32:05 -0500

    Create testRunner.java

---
+++ src/main/java/services/testRunner.java
@@ -0,0 +1,7 @@
+package services;
+
+public class testRunner {
+    public static void main(String[] args) {
+        System.out.println("PASS: " + ParameterService.encryptString("Curtedz@1"));
+    }
+}
