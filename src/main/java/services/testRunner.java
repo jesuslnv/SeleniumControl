f9316fcb4a22c84b5e5425c19c9adf8dd86f3ddf
Jesus Neira <jneiravizcarra@gmail.com>
2019-10-14 14:35:43 -0500

    updated
    updated

--- src/main/java/services/testRunner.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public class testRunner {
-    public static void main(String[] args) {
-        System.out.println("PASS: " + ParameterService.encryptString("Curtedz@1"));
-    }
-}
